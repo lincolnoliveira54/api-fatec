4a69bb94ffedb61f127db3a0d741b3100444d612
lincolnoliveira54 <linconl.oliveira361@gmail.com>
2024-02-29 22:47:23 -0300

    Alteração 29/02/2024

--- src/main/java/br/com/api/fatec/programation/apifatec/ApiFatecApplication.java
+++ src/main/java/br/com/api/fatec/programation/apifatec/ApiFatecApplication.java
@@ -2,10 +2,35 @@ package br.com.api.fatec.programation.apifatec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.websocket.server.PathParam;
+
+@RestController
 
 @SpringBootApplication
 public class ApiFatecApplication {
-
+	
+	@RequestMapping("/")
+	String home() {
+		return "Hello World";
+	}
+	
+	@RequestMapping("/hello")
+	String home2() {
+		return "hello world 2";
+	}
+	
+	@RequestMapping("/numero")
+	Integer home3() {
+		return 25042003;
+	}
+	@RequestMapping("/numero/{num}")
+	Integer home4(@PathVariable Integer num ) {
+		return num;
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ApiFatecApplication.class, args);
 	}
